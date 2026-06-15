3553c491db7f5fb58f50fe3f3827d0e87fb14a2d
Abdallah_Sherif <153302407+Abdallahsheri@users.noreply.github.com>
2026-06-15 05:07:02 +0300

    Implement reverse words functionality in C++

---
+++ Problem #41.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include "MyLib.h"
+#include <iomanip>
+#include <cctype>
+#include <fstream>
+using namespace std;
+//Problem #41 Reverse Words.
+
+string ReadString()
+{
+    string S1;
+    cout << "Please Enter Your String : " << endl;
+    getline(cin, S1);
+    return S1;
+}
+
+vector <string> SplitString(string S1, string Delim)
+{
+    vector <string> vString;
+    short pos = 0; 
+    string sWord;
+
+    while ((pos = S1.find(Delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0,pos);
+        if (sWord != "")
+        {
+            vString.push_back(sWord);
+        }
+        S1.erase(0, pos + Delim.length());
+    }
+    if (S1 != "")
+    {
+        vString.push_back(S1); // it adds last word of the string.
+    }
+    return vString;
+}
+
+string ReverseWordsInString(string S1)
+{
+    vector <string> vString;
+    string S2 = "";
+    vString = SplitString(S1, " ");
+
+    vector <string> ::iterator iter = vString.end();
+    while (iter != vString.begin())
+    {
+        --iter;
+        S2 += *iter + " ";
+    }
+    S2 = S2.substr(0, S2.length() - 1); //remove last space.
+    return S2;
+}
+int main() {
+  
+    string S1 = ReadString();
+    cout << "\n\nString after reversing words:";
+    cout << "\n" << ReverseWordsInString(S1);
+    system("pause>0");
+}
